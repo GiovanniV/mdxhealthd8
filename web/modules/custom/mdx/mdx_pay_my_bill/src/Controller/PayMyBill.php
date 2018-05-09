a6de4535b3887b4815b7105409ceb00a680fbda9
Giovanni <giovanni@vanguardsd.com>
2018-05-09 21:51:56 +0530

    Fix

--- web/modules/custom/mdx/mdx_pay_my_bill/src/Controller/PayMyBill.php
+++ web/modules/custom/mdx/mdx_pay_my_bill/src/Controller/PayMyBill.php
@@ -5,7 +5,7 @@ namespace Drupal\mdx_pay_my_bill\Controller;
 use Drupal\Core\Link;
 use Drupal\Core\Url;
 
-// require_once 'anet_php_sdk/AuthorizeNet.php';
+require_once 'anet_php_sdk/AuthorizeNet.php';
 
 /**
  * Simple page controller for drupal.
@@ -51,25 +51,36 @@ class PayMyBill extends Page {
 		
 		$info = 'Thank you for using MDxHealth Pay My Bill option.<br/>Please enter the patient\'s name, account number and the amount you would like to pay below. You will be directed to a secure page to enter your payment information.';
 		
-		$body = '<p>Please feel free to contact our Billing Department with any questions or concerns on <a href="tel:+1 866.259.5644">+1 866.259.5644</a> or email <a href="https://mail.google.com/mail/?view=cm&amp;fs=1&amp;tf=1&amp;to=patient.first@mdxhealth.com" target="_blank">patient.first@mdxhealth.com</a> Monday to Friday 8AM to 5PM Pacific Time.</p>';
-		
+    $render['info'] = [
+      '#markup' => $info,
+    ];
+    
     if (!isset($_SESSION['display_form']) || !$_SESSION['display_form']) {
 			$PayMyBillForm = \Drupal::formBuilder()->getForm('Drupal\mdx_pay_my_bill\Form\PayMyBillForm');
+      $render['PayMyBillForm'] = $PayMyBillForm;
 		}
     else {
       $amount = $this->pay_my_bill_amount;
       $fp_sequence = $this->pay_my_bill_name . ' - ' . $this->pay_my_bill_account;
-      $forms = \AuthorizeNetDPM::getCreditCardForm($amount, $fp_sequence, '/paymybill/response', $this->loginID, $this->transactionKey, $this->testMode, $this->prefill);
+      $PayMyBillForm = \AuthorizeNetDPM::getCreditCardForm($amount, $fp_sequence, '/paymybill/response', $this->loginID, $this->transactionKey, $this->testMode, $this->prefill);
       $_SESSION['display_form'] = FALSE;
+      
+      
+      $render['form'] = [
+        '#type' => 'html',
+        '#value' => $PayMyBillForm,
+      ];
+      
     }
 		
-		$render[] = array(
-      '#theme' => 'pay_my_bill',
-      '#info' => $info,
-      '#form' => $PayMyBillForm,
-      '#body' => $body,
-    );
+    
+    
+		$body = '<p>Please feel free to contact our Billing Department with any questions or concerns on <a href="tel:+1 866.259.5644">+1 866.259.5644</a> or email <a href="https://mail.google.com/mail/?view=cm&amp;fs=1&amp;tf=1&amp;to=patient.first@mdxhealth.com" target="_blank">patient.first@mdxhealth.com</a> Monday to Friday 8AM to 5PM Pacific Time.</p>';
 		
+    $render['body'] = [
+      '#markup' => $body,
+    ];
+    
     return $render;
   }
 
